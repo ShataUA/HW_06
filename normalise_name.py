a111aa657cbd2be30d468d0af5c0bf5470b33bb1
ShataUA <ShataUA@users.noreply.github.com>
2023-03-06 10:36:05 +0200

    Ready for teacher review Comments included hope:)

--- normalise_name.py
+++ normalise_name.py
@@ -5,9 +5,9 @@ def normalize(file_name):
     CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
     TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
     "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g")
-
-
     TRANS = {}
+
+
     for c, t in zip(CYRILLIC_SYMBOLS, TRANSLATION):
         TRANS[ord(c)] = t  # transliteration small symbols
         TRANS[ord(c.upper())] = t.upper()  # transliteration large symbols
